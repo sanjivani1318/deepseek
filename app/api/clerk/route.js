3c06acc41750b094a1082f2e42d0941c1e5240fb
Sanjivani1318 <jadhavsanjivani480@gmail.com>
2025-06-21 15:44:03 +0530

    updatedheadersinroutejs

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -8,6 +8,8 @@ export async function POST(req){
     const wh=new Webhook(process.env.SIGNING_SECRET)
     const headerPayload = await headers()
     const svixHeaders = {
+        "svix-id":headerPayload.get("svix-id"),
+        "svix-timestamp": headerPayload.get("svix-timestamp"),
         "svix-signature": headerPayload.get("svix-signature"),
 
 
